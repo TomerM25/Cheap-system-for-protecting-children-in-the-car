3b9912cda0b3edda2e9cc0975396248b6b413212
oribn <ori3481@gmail.com>
2020-04-21 10:03:40 +0300

    Alert

---
+++ Alert/app/src/main/java/com/example/ori/alert/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ori.alert;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt=(Button)findViewById(R.id.bt_notification);
+        final MediaPlayer mp=MediaPlayer.create(this,R.raw.loud );
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+                String message = "The child is suing.";
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                        MainActivity.this
+                )
+                        .setSmallIcon(R.drawable.ic_message)
+                        .setContentTitle("New Notification")
+                        .setContentText(message)
+                        .setAutoCancel(true);
+                Intent intent = new Intent(MainActivity.this,NotificationActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("message",message);
+
+                PendingIntent pendingIntent= PendingIntent.getActivity(MainActivity.this,
+                        0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+
+                NotificationManager notificationManager = (NotificationManager)getSystemService(
+                        Context.NOTIFICATION_SERVICE
+                );
+                notificationManager.notify(0,builder.build());
+
+            }
+        });
+    }
+}
