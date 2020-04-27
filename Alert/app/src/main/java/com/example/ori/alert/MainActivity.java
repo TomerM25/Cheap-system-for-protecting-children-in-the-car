25b04af72e818b228f336775141ba9be9f82b62f
TomerM25 <tomer00001@gmail.com>
2020-04-27 20:43:21 +0300

    notif

--- Alert/app/src/main/java/com/example/ori/alert/MainActivity.java
+++ Alert/app/src/main/java/com/example/ori/alert/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.ori.alert;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
--- ProtectYourKids/app/src/main/java/com/example/protectyourkids/MainActivity.java
+++ ProtectYourKids/app/src/main/java/com/example/protectyourkids/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.protectyourkids;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,8 +22,8 @@ import cz.msebera.android.httpclient.Header;
 public class MainActivity extends AppCompatActivity
 {
     private static final long START_TIME_IN_MILLIS = 86400000;      //24 hours
+    private static final String NOTIFICATION_CHANNEL_ID = "Alarm Notification Chanel";
     private TextView txtResponse;
-    private Button bt;
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning;
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
@@ -31,27 +34,44 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.loud);
-        bt = findViewById(R.id.bt_notification);
+        Button bt = findViewById(R.id.bt_notification);
         txtResponse = findViewById(R.id.txtResponse);
-        startTimer();
+        //startTimer();
+
+
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mp.start();
                 String message = "Child is stuning";
-                NotificationCompat.Builder builder= new NotificationCompat.Builder(MainActivity.this)
-                        .setSmallIcon(R.drawable.ic_message)
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+                    Log.d("#####", "Here is the problem");
+                    NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+                    // Configure the notification channel.
+                    notificationChannel.setDescription("Channel description");
+                    notificationChannel.enableLights(true);
+                    //notificationChannel.setLightColor(Color.RED);
+                    notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+                    notificationChannel.enableVibration(true);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, NOTIFICATION_CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_notification)
                         .setContentTitle("New Notification")
                         .setContentText(message)
                         .setAutoCancel(true);
+
                 Intent intent = new Intent(MainActivity.this,NotificationActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 intent.putExtra("message",message);
                 PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
                 builder.setContentIntent(pendingIntent);
-                NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-                notificationManager.notify(0,builder.build());
-
+                notificationManager.notify(0, builder.build());
             }
         });
     }
@@ -79,7 +99,7 @@ public class MainActivity extends AppCompatActivity
     private void newClient()
     {
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://ynet.co.il", new AsyncHttpResponseHandler()
+        client.get("https://ynet.co.il", new AsyncHttpResponseHandler()
         {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
@@ -97,3 +117,11 @@ public class MainActivity extends AppCompatActivity
         });
     }
 }
+
+
+
+
+
+
+
+
